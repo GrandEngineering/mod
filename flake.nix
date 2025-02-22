78fb24b50bc5b0c2203482a817d028076f5d3322
IGN-Styly <claudioresendeptbr@gmail.com>
2025-02-22 19:01:57 +0000

    work?

--- flake.nix
+++ flake.nix
@@ -1,18 +1,82 @@
 {
-  description = "A very basic flake";
-
+  description = "Build a cargo project without extra checks";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane.url = "github:ipetkov/crane";
+
+    flake-utils.url = "github:numtide/flake-utils";
     engine.url = "github:GrandEngineering/engine";
-    nixify.url = "github:rvolosatovs/nixify";
-    engine.flake=false;
+    engine.flake = false;
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs,nixify,... }: {
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    flake-utils,
+    rust-overlay,
+    engine,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      # NB: we don't need to overlay our custom toolchain for the *entire*
+      # pkgs (which would require rebuidling anything else which uses rust).
+      # Instead, we just want to update the scope that crane will use by appending
+      # our specific toolchain there.
+      craneLib = (crane.mkLib pkgs).overrideToolchain (p:
+        p.rust-bin.stable.latest.default.override {
+          targets = ["x86_64-unknown-linux-gnu" "x86_64-pc-windows-gnu"];
+        });
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      my-crate = craneLib.buildPackage {
+        src = craneLib.cleanCargoSource ./.;
+        strictDeps = true;
 
-  };
+        cargoExtraArgs = "--target x86_64-unknown-linux-gnu";
+
+        # Tests currently need to be run via `cargo wasi` which
+        # isn't packaged in nixpkgs yet...
+        doCheck = false;
+
+        buildInputs =
+          [
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            # Additional darwin specific inputs can be set here
+            pkgs.libiconv
+          ];
+      };
+    in {
+      checks = {
+        inherit my-crate;
+      };
+
+      packages.default = my-crate;
+
+      # apps.default = flake-utils.lib.mkApp {
+      #   drv = pkgs.writeShellScriptBin "my-app" ''
+      #     ${pkgs.wasmtime}/bin/wasmtime run ${my-crate}/bin/custom-toolchain.wasm
+      #   '';
+      # };
+
+      devShells.default = craneLib.devShell {
+        # Inherit inputs from checks.
+        checks = self.checks.${system};
+
+        # Extra inputs can be added here; cargo and rustc are provided by default
+        # from the toolchain that was specified earlier.
+        packages = [
+        ];
+      };
+    });
 }
